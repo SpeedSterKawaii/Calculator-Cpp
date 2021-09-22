4841f8199aa96a0d6a94e1862f70308978bd789c
SpeedSterKawaii <72403623+SpeedSterKawaii@users.noreply.github.com>
2021-09-22 10:22:00 -0400

    Create Calculator2Commented.cpp

---
+++ Calculator2Commented.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <Windows.h>
+#include <string>
+using namespace std;
+
+namespace Calculator // namespace to keep it clean :)
+{
+    void DoMath(int d, int e)
+    {
+        std::cout << d + e << std::endl; // print out the integers.
+        d = 0;
+        e = 0;
+    }
+    
+    void DoSubtract(int a, int b)
+    {
+        std::cout << a - b << std::endl;
+        Sleep(100);
+        a = 0;
+        b = 0;
+    }
+}
+
+int main(){
+    Calculator::DoMath(5, 5); // return "10"
+    Calculator::DoSubtract(10, 4); // returns "6"
+    getchar();
+    exit(0);
+    return NULL;
+}
