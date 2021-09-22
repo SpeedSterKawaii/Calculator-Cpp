30ba1474ae765e31c9b0aa3a4058938ba382e198
SpeedSterKawaii <72403623+SpeedSterKawaii@users.noreply.github.com>
2021-09-22 10:29:49 -0400

    Update Calculator2Commented.cpp

--- Calculator2Commented.cpp
+++ Calculator2Commented.cpp
@@ -5,18 +5,18 @@ using namespace std;
 
 namespace Calculator // namespace to keep it clean :)
 {
-    void DoMath(int d, int e)
+    void DoMath(int d, int e) // we will add the int's d + int e.
     {
         std::cout << d + e << std::endl; // print out the integers.
-        d = 0;
+        d = 0; // clear it so we wont add the same exact thing :D
         e = 0;
     }
     
-    void DoSubtract(int a, int b)
+    void DoSubtract(int a, int b) // use - for subtraction.
     {
-        std::cout << a - b << std::endl;
-        Sleep(100);
-        a = 0;
+        std::cout << a - b << std::endl; // subtract b from a.
+        Sleep(100); // remove if you are big brain :D
+        a = 0;//as again change the integer into NULL;
         b = 0;
     }
 }
@@ -24,7 +24,7 @@ namespace Calculator // namespace to keep it clean :)
 int main(){
     Calculator::DoMath(5, 5); // return "10"
     Calculator::DoSubtract(10, 4); // returns "6"
-    getchar();
-    exit(0);
-    return NULL;
+    getchar();//when someone types a letter and clicks enter.
+    exit(0);//kill the process.
+    return NULL;//need return since its a int.
 }
